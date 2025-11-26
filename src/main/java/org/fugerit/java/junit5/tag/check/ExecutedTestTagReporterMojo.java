bca09e40146cfeb995c83de6bfa93591ce63a28e
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-11-26 21:11:22 +0100

    chore: refactor ExecutedTestTagReporterMojo - tag surefire and check facade

--- src/main/java/org/fugerit/java/junit5/tag/check/ExecutedTestTagReporterMojo.java
+++ src/main/java/org/fugerit/java/junit5/tag/check/ExecutedTestTagReporterMojo.java
@@ -5,23 +5,16 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.*;
 import org.apache.maven.project.MavenProject;
-import org.dom4j.Document;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
+import org.fugerit.java.junit5.tag.check.facade.TagCheckFacade;
 import org.fugerit.java.junit5.tag.check.facade.TagReportFacade;
 import org.fugerit.java.junit5.tag.check.facade.TagScanFacade;
+import org.fugerit.java.junit5.tag.check.facade.TagSurefireFacade;
 import org.fugerit.java.junit5.tag.check.model.ExecutedTest;
-import org.fugerit.java.junit5.tag.check.model.TestStats;
-import org.junit.jupiter.api.Tag;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Reports tags from actually executed tests by parsing Surefire reports
@@ -71,7 +64,7 @@ public class ExecutedTestTagReporterMojo extends AbstractMojo {
             URLClassLoader classLoader = createTestClassLoader();
 
             // Parse Surefire reports to find executed tests
-            List<ExecutedTest> executedTests = parseSurefireReports();
+            List<ExecutedTest> executedTests = TagSurefireFacade.parseSurefireReports( this.surefireReportsDirectory, this.includeSkipped );
 
             getLog().info("Found " + executedTests.size() + " executed tests");
 
@@ -84,7 +77,7 @@ public class ExecutedTestTagReporterMojo extends AbstractMojo {
 
             // Check for required tags
             if (requiredTags != null && !requiredTags.isEmpty()) {
-                checkRequiredTags(testTagMap);
+                TagCheckFacade.checkRequiredTags( this.requiredTags, this.failOnMissingTag, testTagMap);
             }
 
             getLog().info("Executed Test Tag Report generated: " +
@@ -110,91 +103,6 @@ public class ExecutedTestTagReporterMojo extends AbstractMojo {
                 .toArray(URL[]::new);
 
         return new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
-    }
-
-    private List<ExecutedTest> parseSurefireReports() {
-        List<ExecutedTest> executedTests = new ArrayList<>();
-        SAXReader reader = new SAXReader();
-
-        // Find all XML report files
-        File[] reportFiles = surefireReportsDirectory.listFiles(
-                (dir, name) -> name.startsWith("TEST-") && name.endsWith(".xml"));
-
-        if (reportFiles == null || reportFiles.length == 0) {
-            getLog().warn("No Surefire XML reports found in: " +
-                    surefireReportsDirectory.getAbsolutePath());
-            return executedTests;
-        }
-
-        for (File reportFile : reportFiles) {
-            getLog().debug("Parsing report: " + reportFile.getName());
-
-            try {
-                Document document = reader.read(reportFile);
-                Element root = document.getRootElement();
-
-                String className = root.attributeValue("name");
-
-                @SuppressWarnings("unchecked")
-                List<Element> testCases = root.elements("testcase");
-
-                for (Element testCase : testCases) {
-                    String methodName = testCase.attributeValue("name");
-                    String testClassName = testCase.attributeValue("classname", className);
-                    String time = testCase.attributeValue("time");
-
-                    boolean skipped = testCase.element("skipped") != null;
-                    boolean failed = testCase.element("failure") != null;
-                    boolean error = testCase.element("error") != null;
-
-                    ExecutedTest test = new ExecutedTest(
-                            testClassName,
-                            methodName,
-                            skipped,
-                            failed,
-                            error,
-                            time
-                    );
-
-                    if (!skipped || includeSkipped) {
-                        executedTests.add(test);
-                    }
-                }
-            } catch (Exception e) {
-                getLog().warn("Error parsing report file: " + reportFile.getName(), e);
-            }
-        }
-
-        return executedTests;
-    }
-
-
-
-    private void checkRequiredTags(Map<ExecutedTest, Set<String>> testTagMap)
-            throws MojoExecutionException {
-        Set<String> foundTags = testTagMap.values().stream()
-                .flatMap(Set::stream)
-                .collect(Collectors.toSet());
-
-        List<String> missingTags = new ArrayList<>();
-        for (String requiredTag : requiredTags) {
-            if (!foundTags.contains(requiredTag)) {
-                missingTags.add(requiredTag);
-            }
-        }
-
-        if (!missingTags.isEmpty()) {
-            String message = "Missing required tags in executed tests: " +
-                    String.join(", ", missingTags);
-            if (failOnMissingTag) {
-                throw new MojoExecutionException(message);
-            } else {
-                getLog().warn(message);
-            }
-        } else {
-            getLog().info("All required tags found in executed tests: " +
-                    String.join(", ", requiredTags));
-        }
     }
 
 }
