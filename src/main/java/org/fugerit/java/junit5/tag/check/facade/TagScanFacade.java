33b2526fd4da7600e8f26ffa2a555e55311f1f76
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-12-02 15:46:05 +0100

    chore: more coverage

--- src/main/java/org/fugerit/java/junit5/tag/check/facade/TagScanFacade.java
+++ src/main/java/org/fugerit/java/junit5/tag/check/facade/TagScanFacade.java
@@ -5,7 +5,6 @@ import org.fugerit.java.junit5.tag.check.model.ExecutedTest;
 import org.junit.jupiter.api.Tag;
 
 import java.lang.reflect.Method;
-import java.net.URLClassLoader;
 import java.util.*;
 
 @Slf4j
@@ -15,30 +14,14 @@ public class TagScanFacade {
 
     public static Map<ExecutedTest, Set<String>> extractTagsFromExecutedTests(
             List<ExecutedTest> executedTests,
-            URLClassLoader classLoader) {
-
+            ClassLoader classLoader) {
         Map<ExecutedTest, Set<String>> testTagMap = new LinkedHashMap<>();
-
         for (ExecutedTest test : executedTests) {
             try {
                 Class<?> testClass = classLoader.loadClass(test.getClassName());
                 Method testMethod = findTestMethod(testClass, test.getMethodName());
-
                 if (testMethod != null) {
-                    Set<String> tags = new HashSet<>();
-
-                    // Get tags from method
-                    Tag[] methodTags = testMethod.getAnnotationsByType(Tag.class);
-                    for (Tag tag : methodTags) {
-                        tags.add(tag.value());
-                    }
-
-                    // Get tags from class
-                    Tag[] classTags = testClass.getAnnotationsByType(Tag.class);
-                    for (Tag tag : classTags) {
-                        tags.add(tag.value());
-                    }
-
+                    Set<String> tags = getMethodTag(testMethod, testClass);
                     testTagMap.put(test, tags);
                 } else {
                     log.warn("Could not find method: {} #{}", test.getClassName(), test.getMethodName());
@@ -49,8 +32,22 @@ public class TagScanFacade {
                 testTagMap.put(test, Collections.emptySet());
             }
         }
-
         return testTagMap;
+    }
+
+    private static Set<String> getMethodTag(Method testMethod, Class<?> testClass) {
+        Set<String> tags = new HashSet<>();
+        // Get tags from method
+        Tag[] methodTags = testMethod.getAnnotationsByType(Tag.class);
+        for (Tag tag : methodTags) {
+            tags.add(tag.value());
+        }
+        // Get tags from class
+        Tag[] classTags = testClass.getAnnotationsByType(Tag.class);
+        for (Tag tag : classTags) {
+            tags.add(tag.value());
+        }
+        return tags;
     }
 
     private static Method findTestMethod(Class<?> testClass, String methodName) {
---
+++ src/test/java/org/fugerit/java/junit5/tag/check/TagScanFacadeTest.java
@@ -0,0 +1,40 @@
+package org.fugerit.java.junit5.tag.check;
+
+import org.fugerit.java.junit5.tag.check.facade.TagScanFacade;
+import org.fugerit.java.junit5.tag.check.model.ExecutedTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+class TagScanFacadeTest {
+
+    @Test
+    void testClassNotFound() {
+        List<ExecutedTest> executedTests = new ArrayList<>();
+        ExecutedTest test = new ExecutedTest( "acme.MyClass", "myMethod", Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, BigDecimal.ONE );
+        executedTests.add( test );
+        Map<ExecutedTest, Set<String>>  testTagMap = TagScanFacade.extractTagsFromExecutedTests( executedTests, Thread.currentThread().getContextClassLoader() );
+        Assertions.assertTrue( testTagMap.get( test ).isEmpty() );
+    }
+
+    @Test
+    void testMethodNotFound() {
+        List<ExecutedTest> executedTests = new ArrayList<>();
+        ExecutedTest test = new ExecutedTest( TagScanFacadeTest.class.getName(), "myMethod", Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, BigDecimal.ONE );
+        executedTests.add( test );
+        Map<ExecutedTest, Set<String>>  testTagMap = TagScanFacade.extractTagsFromExecutedTests( executedTests, Thread.currentThread().getContextClassLoader() );
+        Assertions.assertTrue( testTagMap.get( test ).isEmpty() );
+    }
+
+    @Test
+    void testTagNotFound() {
+        List<ExecutedTest> executedTests = new ArrayList<>();
+        ExecutedTest test = new ExecutedTest( TagScanFacadeTest.class.getName(), "testMethodNotFound", Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, BigDecimal.ONE );
+        executedTests.add( test );
+        Map<ExecutedTest, Set<String>>  testTagMap = TagScanFacade.extractTagsFromExecutedTests( executedTests, Thread.currentThread().getContextClassLoader() );
+        Assertions.assertTrue( testTagMap.get( test ).isEmpty() );
+    }
+
+}
